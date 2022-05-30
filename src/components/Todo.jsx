924a15c1eb6301d3e919ecc37b0107c124df6301
Waheeda Dudekula <dwaheeda@gmail.com>
2022-05-30 10:33:25 -0500

    memory leak sample completed with useRef and return useRef when unmounted

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,15 +4,28 @@ function Todo() {
   const [loading, setLoading] = useState(true)
   const [todo, setTodo] = useState({})
 
+  const isMounted = useRef(true)
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then((res) => res.json())
-      .then((data) => {
-        setTimeout(() => {
-          setTodo(data)
-          setLoading(false)
-        }, 3000)
-      })
+      .then(
+        (data) => {
+          setTimeout(() => {
+            if (isMounted.current) {
+              setTodo(data)
+              setLoading(false)
+            }
+          }, 3000)
+        },
+        [isMounted]
+      )
+
+    //This runs when component is unmounted
+    return () => {
+      console.log(123)
+      isMounted.current = false
+    }
   }, [])
   return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>
 }
